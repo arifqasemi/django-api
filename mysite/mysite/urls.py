f505715761017e0773552b8b4d44ecfbf8e84725
arif qasemi <arifsultani1998@gmail.com>
2023-10-15 17:08:58 +0700

    ghanbe

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/', include('app.urls')),
-    path('api/', include('app.urls', namespace='app')),
+    # path('api/', include('app.urls', namespace='app')),
 
 
 ]
