8096d0f28f140cf994ac85255f3fb5975e343d84
arif qasemi <arifsultani1998@gmail.com>
2023-10-16 07:14:54 +0700

    fix urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('app.urls')),
+    path('api/', include('app.urls'))
     # path('api/', include('app.urls', namespace='app')),
 
 
