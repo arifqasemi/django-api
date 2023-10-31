5d62d68d0932c0ae8db574590edfdc162f99ce84
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2023-10-31 07:40:51 +0700

    main

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path
-# from app.views import QuestionsView,AnswerView,QuestionDetail,AnswerDetail,TestView,LessonsView,QuestionsMAView,MultiAnswerView,TestMultiAnswer
+from app.views import QuestionsView,AnswerView,QuestionDetail,AnswerDetail,TestView,LessonsView,QuestionsMAView,MultiAnswerView,TestMultiAnswer
 from . import views
 app_name = 'app' 
 urlpatterns = [
-    path('home/',views.test)
-    # path('lessons/',LessonsView.as_view(),name='lessons' ),
-    # path('questions/',QuestionsView.as_view(),name='questions' ),
-    # path('question/<int:pk>',QuestionDetail.as_view(),name='question-detail' ),
-    # path('multi-answer-questions/',QuestionsMAView.as_view(),name='multi-answer-questions' ),
+    path('lessons/',LessonsView.as_view(),name='lessons' ),
+    path('questions/',QuestionsView.as_view(),name='questions' ),
+    path('question/<int:pk>',QuestionDetail.as_view(),name='question-detail' ),
+    path('multi-answer-questions/',QuestionsMAView.as_view(),name='multi-answer-questions' ),
 
-    # path('answers/',AnswerView.as_view(),name='answers' ),
-    # path('answer/<int:pk>',AnswerDetail.as_view(),name='answer-detail' ),
-    # path('test/',TestView.as_view(),name='test' ),
-    # path('multianswer/<int:pk>',AnswerDetail.as_view(),name='multianswer-detail' ),
-    # path('multianswer/',MultiAnswerView.as_view(),name='multianswer' ),
-    # path('multi-answer-test/',TestMultiAnswer.as_view(),name='multi-answer-test' ),
+    path('answers/',AnswerView.as_view(),name='answers' ),
+    path('answer/<int:pk>',AnswerDetail.as_view(),name='answer-detail' ),
+    path('test/',TestView.as_view(),name='test' ),
+    path('multianswer/<int:pk>',AnswerDetail.as_view(),name='multianswer-detail' ),
+    path('multianswer/',MultiAnswerView.as_view(),name='multianswer' ),
+    path('multi-answer-test/',TestMultiAnswer.as_view(),name='multi-answer-test' ),
 
 
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-s5c()#l8mp9g0wdz1hi(rzmyl$ce71bmiozidh4opbxw1@f++2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['arifqasemi12.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    # 'rest_framework',
+    'rest_framework',
 
 ]
 
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
